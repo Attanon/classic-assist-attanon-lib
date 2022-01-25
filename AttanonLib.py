50809be9645045f848b5d61632225a1635c408ec
Jakub Vrchota <jamesvradio@gmail.com>
2022-01-25 20:46:50 +0100

    First version of AttanonLib, add AttanonTypes

--- AttanonLib.py
+++ AttanonLib.py
@@ -3,6 +3,8 @@
 #AttanonLib
 Version = "0.0.1"
 
+from AttanonTypes import *
+
 from Assistant import Engine
 from ClassicAssist.Data.Macros.Commands.AbilitiesCommands import *
 from ClassicAssist.Data.Macros.Commands.ActionCommands import *
@@ -31,3 +33,60 @@ from ClassicAssist.Data.Macros.Commands.TargetCommands import *
 from ClassicAssist.Data.Macros.Commands.TimerCommands import *
 from ClassicAssist.Data.Macros.Commands.WandCommands import *
 
+import os
+import math
+
+def GetType(itemTypeName):
+    if itemTypeName in Graphics:
+        return Graphics[itemTypeName]
+    elif itemTypeName in Types:
+        return Types[itemTypeName]
+
+    return 0
+
+def FindTypeBy(itemType, container=None):
+    items = []
+    if container is not None:
+        cont = Engine.Items.GetItem(container)
+
+        if cont.Container == None:
+            WaitForContents(container, 5000)
+
+        items = cont.Container.GetItems()
+    else:
+        items = Engine.Items.GetAllItems()
+
+
+    returnItems = []
+    if isinstance(itemType, ItemTypeClass):
+        for item in items:
+            if itemType.match(item):
+                returnItems.append(item)
+    elif isinstance(itemType, str):
+        itemType = GetType(itemType)
+        if itemType == 0:
+            print("Type " + itemType + " neexistuje")
+            return 0
+
+        return FindTypeBy(itemType, container)
+    else:
+        for item in items:
+            if item.ID == itemType:
+                returnItems.append(item)
+
+    if returnItems == []:
+        return None
+
+    return returnItems[0]
+
+
+def MoveItem(type, src, tar, amount=1, color=-1):
+    if CountType(type, src) <= 0:
+        return False
+
+    FindType(type, -1, src, color)
+    MoveItem("found", tar, amount)
+    return True
+
+def Distance(x1, y1, x2, y2):
+    return math.sqrt(((x2 - x1) ** 2) + ((y2 - y1) ** 2))
